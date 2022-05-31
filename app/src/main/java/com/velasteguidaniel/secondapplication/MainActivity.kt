1b54602f46d0a4557e0de38760c5a854e5145ae3
Daniel Velastegui <55715214+danielvelastegui@users.noreply.github.com>
2022-05-31 17:52:28 -0500

    Toast

--- app/src/main/java/com/velasteguidaniel/secondapplication/MainActivity.kt
+++ app/src/main/java/com/velasteguidaniel/secondapplication/MainActivity.kt
@@ -2,10 +2,12 @@ package com.velasteguidaniel.secondapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this, "Hola mundo", Toast.LENGTH_SHORT).show()
     }
 }
